887b60dab1379268331f15e6752677ffb19d378f
a2un <encodedgeek@gmail.com>
2026-04-18 14:50:26 -0400

    update static

--- ereader/settings.py
+++ ereader/settings.py
@@ -136,7 +136,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/ereader/static/'
-STATIC_ROOT = BASE_DIR + '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 BASE_URL = BASE_DIR
 
 # MongoDB Database
